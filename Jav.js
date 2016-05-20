f6bc4f72d412a4fd8d1d0fcd8328ed48db698e1f
KitsuneNoctus <theceintiant@gmail.com>
2016-05-20 07:56:23 -0700

    Add files via upload

---
+++ Jav.js
@@ -0,0 +1,10 @@
+
+var newBeg =  function(){
+  var sum = 0;
+  for(var i = 0; i< den.length ;i++){
+    sum += den[i];
+  }
+  return sum/den.length;
+}
+
+window.alert("Oh no you are pathetic haha")
